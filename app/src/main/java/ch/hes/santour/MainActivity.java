d0bc97f6e31d0a508b78ceba411112f67b50bed6
kevdlc <32674096+kevdlc@users.noreply.github.com>
2017-11-21 16:13:51 +0100

    commit objects BD

---
+++ app/src/main/java/ch/hes/santour/MainActivity.java
@@ -0,0 +1,44 @@
+package ch.hes.santour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //button create track
+        Button buttonCreate = (Button) findViewById(R.id.createTrack);
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CreateTrackActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // button about
+        Button buttonAbout = (Button) findViewById(R.id.about);
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
