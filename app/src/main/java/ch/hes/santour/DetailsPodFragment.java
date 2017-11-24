170fc5695cb81039ce25458cbbf5cfece6ce2ca5
audreymichel <audreyceliamichel@gmail.com>
2017-11-24 14:33:07 +0100

    commit

--- app/src/main/java/ch/hes/santour/DetailsPodFragment.java
+++ app/src/main/java/ch/hes/santour/DetailsPodFragment.java
@@ -23,7 +23,6 @@ public class DetailsPodFragment extends Fragment {
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
