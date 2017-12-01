7e77bfd7e4714c56bb68910da00bd392c5e03b02
kevdlc <32674096+kevdlc@users.noreply.github.com>
2017-12-01 12:43:29 +0100

    appareil photo sur add POI

--- app/src/main/java/ch/hes/santour/CreatePoiFragment.java
+++ app/src/main/java/ch/hes/santour/CreatePoiFragment.java
@@ -1,30 +1,36 @@
 package ch.hes.santour;
 
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import BLL.POIManager;
 
 
 public class CreatePoiFragment extends Fragment {
-
+    private final int CAMERA_REQUEST = 1;
     FragmentManager fragmentManager;
     Fragment fragment;
-    FragmentTransaction transaction ;
+    FragmentTransaction transaction;
 
     private EditText poiDescription;
     private EditText poiName;
     private POIManager poiManager;
+    private ImageView imageView;
 
     public CreatePoiFragment() {
 
@@ -37,17 +43,18 @@ public class CreatePoiFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_create_poi, container, false);
-
+        imageView = (ImageView) rootView.findViewById(R.id.iv_poi_img);
         //We set up poi informations
         poiName = rootView.findViewById(R.id.et_poi_name);
         poiDescription = rootView.findViewById(R.id.et_poi_description);
         poiManager = new POIManager();
 
+
         //set the title on the app
         getActivity().setTitle(R.string.create_poi);
 
         //button CANCEL
-        Button bt_poi_cancel =  rootView.findViewById(R.id.bt_poi_cancel);
+        Button bt_poi_cancel = rootView.findViewById(R.id.bt_poi_cancel);
         bt_poi_cancel.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -61,16 +68,26 @@ public class CreatePoiFragment extends Fragment {
 
             public void onClick(View v) {
 
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,CAMERA_REQUEST);
+
 
             }
         });
 
 
-
-
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            Log.i("YOLOYOLO", "YOLO :" + data);
+            imageView.setImageBitmap(photo);
+        }
     }
 
 
